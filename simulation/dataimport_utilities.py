0a9ff758bf583fd267b32cca9d0ca9c801fc9750
Blazakin <felschf05@gmail.com>
2025-10-26 21:07:26 -0700

    added sample .eng thrust file and .eng reader

---
+++ simulation/dataimport_utilities.py
@@ -0,0 +1,41 @@
+import json
+import csv
+# import pandas as pd
+
+def read_thrust_data(filename):
+    array = []
+    with open(filename, "r") as file:
+        for line in csv.reader(file):
+            array += [line]
+    return array
+
+def read_eng_thrustfile(filename):
+    array = []
+    with open(filename, "r") as file:
+        is_header = 1
+        for line in file:
+            if ";" in line:
+                continue
+            if is_header:
+                is_header = 0
+                header_values = line.strip().split(" ")
+                if len(header_values) < 7:
+                    return -1
+                header_line = {"motor_name":header_values[0],
+                               "diameter":header_values[1],
+                               "length":header_values[2],
+                               "delays":header_values[3],
+                               "prop_weight":header_values[4],
+                               "tot_weight":header_values[5],
+                               "manufacturer":header_values[6]}
+                array += [header_line]
+            else:
+                array += [line.strip().split(" ")]
+    return array
+
+if __name__ == "__main__":
+    print(read_eng_thrustfile("sample_datasets/AeroTech_N2000W.eng"))
+
+
+
+
