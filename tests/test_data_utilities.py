c52f512d7651e8f5399aff051c4135a872df44a0
Blazakin <felschf05@gmail.com>
2025-11-17 00:36:13 -0800

    fix: corrected sample file paths

--- tests/test_data_utilities.py
+++ tests/test_data_utilities.py
@@ -2,18 +2,19 @@ import filecmp
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "data_utilities"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "data_utilities"))
 from eng_to_csv import eng_to_csv
 
 
 
 def test_eng_to_csv():
-    eng_src = "../sample_datasets/AeroTech_N2000W.eng"
+    eng_src = os.path.join(os.path.dirname(__file__), "..", "sample_datasets", "AeroTech_N2000W.eng")
 
-    spec_true = "../sample_datasets/motor_spec.csv"
-    spec_created = "../sample_datasets/motor_spec_test.csv"
+    spec_true = os.path.join(os.path.dirname(__file__), "..", "sample_datasets", "motor_spec.csv")
+    spec_created = os.path.join(os.path.dirname(__file__), "..", "sample_datasets", "motor_spec_test.csv")
 
-    thrust_true = "../sample_datasets/thrust_motor.csv"
-    thrust_created = "../sample_datasets/thrust_motor_test.csv"
+    thrust_true = os.path.join(os.path.dirname(__file__), "..", "sample_datasets", "thrust_motor.csv")
+    thrust_created = os.path.join(os.path.dirname(__file__), "..", "sample_datasets", "thrust_motor_test.csv")
     eng_to_csv(eng_src, thrust_created, spec_created)
 
     assert(filecmp.cmp(spec_true, spec_created, shallow=False))
@@ -24,5 +25,3 @@ def test_eng_to_csv():
     if os.path.exists(thrust_created):
         os.remove(thrust_created)
 
-
-test_eng_to_csv()
