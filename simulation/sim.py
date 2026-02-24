1b7019ae7f7bf5177c839cfbfb2a4ee3fd9e3faa
Noah Unger-Schulz <noahungerschulz@gmail.com>
2026-02-24 15:12:18 -0800

    graphs of different intial angles

--- simulation/sim.py
+++ simulation/sim.py
@@ -45,13 +45,13 @@ def Accel(t,h,v,exti):
 
 #FDS bs
 def Fext(t,u,exti):#the derivative of the state space
-    return np.array([u[1],Accel(t,u[0],u[1],exti)])
+    return np.array([u[1]*cos(u[2]),Accel(t,u[0],u[1],exti),-g*sin(u[2])/(u[1]+Accel(t,u[0],u[1],exti))])
 
 
 def run_integrate_adaptive(dt,exti=3,t0=0):
     T=200
     dt=.05
-    U0=np.array([0,0])#u[0]=height u[1]=velocity
+    U0=np.array([0,0,1/15])#u[0]=height u[1]=velocity
     def F(t,u):
        return Fext(t,u,exti) 
     return integrate_adaptive([U0,U0],Heun,F,T,dt,t0=t0)
@@ -72,7 +72,7 @@ def apogee(exti,U0,T,t0,dt=.0005):
     U,Time=run_scipy(dt,exti=exti,t0=t0,U0=U0)
     return np.max(U[:,0])
 
-def run(headless=False,exti=3,U0=np.array([0,0]),t0=4):
+def run(headless=False,exti=3,U0=np.array([0,0,1/15]),t0=4):
 #initial conditions
     T=200
     dt=.05
@@ -83,8 +83,9 @@ def run(headless=False,exti=3,U0=np.array([0,0]),t0=4):
 
     if not headless:
         # plotting
-        plt.plot(Time45,U45[:,0],label="scipyh")
-        plt.plot(Time45,U45[:,1],label="scipyv")
+        plt.plot(Time45,U45[:,0],label="scipyh,exti="+str(exti))
+        plt.plot(Time45,U45[:,1],label="scipyv,exti="+str(exti))
+        plt.plot(Time45,U45[:,2]*1000,label="scipytheta,exti="+str(exti))
         plt.plot(Time45,[10000/3.3 for t in Time45],label="goalheight")
         
         # plt.plot(Time,U[:,0],label="Height")
@@ -101,29 +102,41 @@ def mach2v(V):
     return [343.*v for v in V]
 
 if __name__ == '__main__':
-    # run(exti=0,U0=np.array([0,0]),t0=0)
-    # run(exti=1,U0=np.array([0,0]),t0=0)
-    # run(exti=2,U0=np.array([0,0]),t0=0)
-    # run(exti=3,U0=np.array([0,0]),t0=0)
-    # plt.show()
-    heights=[30*i+2000 for i in range(30)]
-    machs=[0.1+0.03*i for i in range(30)]
-    print("Heights:",heights)
-    print("Machs:",machs)
-    lookup=[[] for hi in heights]
-    for hi in range(len(heights)):
-        for vi in range(len(machs)):
-            U0=np.array([heights[hi],mach2v(machs)[vi]])
-            # plt.clf()
-            # run(exti=0,U0=U0)
-            # run(exti=1,U0=U0)
-            # run(exti=2,U0=U0)
-            # run(exti=3,U0=U0)
-            # plt.pause(10**-100)
-            apogees=[]
-            for exti in range(len(Exts)):
-                apogees+=[abs(apogee(exti,U0,200,t0=4)-10000/3.3)]
-            print(U0,[float(a) for a in apogees])
-            plt.show()
-            lookup[hi]+=[int(np.array(apogees).argmin())]
-    print("Lookup:",np.array(lookup))
+    if False:
+        run(exti=0,U0=np.array([0,0,1/15]),t0=0)
+        run(exti=1,U0=np.array([0,0,1/15]),t0=0)
+        run(exti=2,U0=np.array([0,0,1/15]),t0=0)
+        run(exti=3,U0=np.array([0,0,1/15]),t0=0)
+        plt.show()
+    elif True:
+        run(exti=0,U0=np.array([0,0,pi/180]),t0=0)
+        run(exti=0,U0=np.array([0,0,0]),t0=0)
+        plt.show()
+    else:
+        heights=[200*i+2000 for i in range(5)]
+        machs=[0.1+0.2*i for i in range(5)]
+        angles=[pi/180*i**2 for i in range(4)]
+        print("Angles:",heights)
+        print("Heights:",heights)
+        print("Machs:",machs)
+        lookup=[[[] for ai in angles] for hi in heights]
+
+        for hi in range(len(heights)):
+            for ai in range(len(angles)):
+                for vi in range(len(machs)):
+                    U0=np.array([heights[hi],mach2v(machs)[vi],angles[ai]])
+                    # plt.clf()
+                    # run(exti=0,U0=U0)
+                    # run(exti=1,U0=U0)
+                    # run(exti=2,U0=U0)
+                    # run(exti=3,U0=U0)
+                    # plt.pause(10**-100)
+                    apogees=[]
+                    for exti in range(len(Exts)):
+                        apogees+=[abs(apogee(exti,U0,200,t0=4)-10000/3.3)]
+                    print(U0,[float(a) for a in apogees])
+                    plt.show()
+                    lookup[hi][ai]+=[int(np.array(apogees).argmin())]
+        print("Lookup:")
+        print(np.array(lookup))
+    
