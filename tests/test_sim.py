5f2dd343a09f481f79a110f391e7454693fe3bbe
Noah Unger-Schulz <noahungerschulz@gmail.com>
2026-03-09 17:36:39 -0700

    got rid of useless test

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -7,11 +7,5 @@ def test_run():
     sim.run(headless=True)
 
 
-def test_adaptive_convergence():
-    conv.conv_test(
-        lambda x: max(sim.run_integrate_adaptive(5 * (0.1**x))[1]),
-        5,
-        conv.power_bound(-1),
-    )
 
 
