a994333082312c4de4edde8b4b3e3a517a7bc71a
Forrest <felschf05@gmail.com>
2026-03-15 21:08:50 -0700

    fix: another overwritten variable fixed

--- simulation/integration.py
+++ simulation/integration.py
@@ -15,7 +15,7 @@ def scipyintegrate(u0, scheme, f, max_t, dt, t0=0):
     u = [u0]
     t = [t0]
     solution = scheme(f, t0, u0, max_t, max_step=dt)
-    while u[-1][1] >= -10 and t[-1] < t:  #
+    while u[-1][1] >= -10 and t[-1] < max_t:  #
         solution.step()
         t += [solution.t]
         u += [solution.y]
